46eb1f0c249829321c4fb5fbdf8a6e9fe35ff15c
alireza <alireza.ksv@gmail.com>
2022-01-20 14:37:59 +0330

    added send message

---
+++ public/js/main.js
@@ -0,0 +1,38 @@
+const chatForm = document.getElementById("chat-form");
+const chatMessages = document.querySelector(".chat-messages");
+
+const socket = io();
+
+socket.on("message", (message) => {
+  console.log(message);
+  sendMessage(message);
+
+  // scroll last message
+  chatMessages.scrollTop = chatMessages.scrollHeight;
+});
+
+// Send message
+
+chatForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const msg = e.target.elements.msg.value;
+
+  socket.emit("chatMessage", msg);
+
+  // clear input
+
+  e.target.elements.msg.value = "";
+  e.target.elements.msg.focus();
+});
+
+function sendMessage(message) {
+  const div = document.createElement("div");
+  div.classList.add("message");
+  div.innerHTML = `<p class="meta">User: <span>time</span></p>
+    <p class="text">
+        ${message}
+    </p>
+  `;
+  document.querySelector(".chat-messages").appendChild(div);
+}
--- server.js
+++ server.js
@@ -1,10 +1,34 @@
 const express = require("express");
 const path = require("path");
+const http = require("http");
+const socketio = require("socket.io");
 
 const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
 
 app.use(express.static(path.join(__dirname, "public")));
 
+io.on("connection", (socket) => {
+  console.log("New User Connection ...");
+
+  socket.emit("message", "Welcome User");
+
+  // When another user connects
+  socket.broadcast.emit("message", "A user has joined the chat");
+
+  // When user disconnects
+  socket.on("disconnect", () => {
+    io.emit("message", "A user has left the chat");
+  });
+
+  // Listen for chats
+
+  socket.on("chatMessage", (msg) => {
+    io.emit("message", msg);
+  });
+});
+
 const PORT = 8000 || process.env.PORT;
 
-app.listen(PORT, () => console.log("Server is runnig on port " + PORT));
+server.listen(PORT, () => console.log("Server is runnig on port " + PORT));
