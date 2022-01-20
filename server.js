11acea117ba85a0cca02a69249616cc6c7396c51
alireza <alireza.ksv@gmail.com>
2022-01-20 14:48:25 +0330

    added message format, username, time, text

--- server.js
+++ server.js
@@ -2,30 +2,33 @@ const express = require("express");
 const path = require("path");
 const http = require("http");
 const socketio = require("socket.io");
+const formatMessage = require("./utils/messages");
 
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
+const botName = "admin";
 
 app.use(express.static(path.join(__dirname, "public")));
 
 io.on("connection", (socket) => {
-  console.log("New User Connection ...");
-
-  socket.emit("message", "Welcome User");
+  socket.emit("message", formatMessage(botName, "Welcome User"));
 
   // When another user connects
-  socket.broadcast.emit("message", "A user has joined the chat");
+  socket.broadcast.emit(
+    "message",
+    formatMessage(botName, "A user has joined the chat")
+  );
 
   // When user disconnects
   socket.on("disconnect", () => {
-    io.emit("message", "A user has left the chat");
+    io.emit("message", formatMessage(botName, "A user has left the chat"));
   });
 
   // Listen for chats
 
   socket.on("chatMessage", (msg) => {
-    io.emit("message", msg);
+    io.emit("message", formatMessage("user", msg));
   });
 });
 
